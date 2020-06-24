91bd3b32d698a0b228d0809cc61f3661cb0adba7
VVErshov <vvershov@ibs.ru>
2020-06-24 10:30:27 +0500

    checked the ability to add styles in js (ie 11)

--- emails-input.js
+++ emails-input.js
@@ -1,3 +1,18 @@
+function EmailsInput(inputContainer, settings) {
+    var parser = new DOMParser(),
+        emailsForm = parser.parseFromString(templates.main(), "text/html"),
+        documentStyle = document.createElement('style'),
+        style = templates.style();
+
+    inputContainer.appendChild(emailsForm.body.firstElementChild);
+    document.head.appendChild(documentStyle);
+
+    // TODO раскомментировать, когда будет сделаны все стили emails-form
+    // for (var i = 0; i < style.length; i++) {
+    //     documentStyle.sheet.insertRule(style[i], i);
+    // }
+}
+
 var templates = {
     main: function () {
         return '<div class="emails-form" \
@@ -8,81 +23,22 @@ var templates = {
                         aria-placeholder="add more people..."></span> \
                 </div> \
         ';
-    }
+    },
+
+    tag: function () {
+
+    },
+    // TODO раскомментировать, когда будет сделаны все стили
+    //  emails-form, новые стили добавлять в этот массив
+    // style: function () {
+    //     return [
+    //         '.emails-form {background: #FFFFFF; border: 1px solid #C3C2CF; box-sizing: border-box; border-radius: 4px; ' +
+    //             'min-height: 96px; max-height: 96px; }',
+    //         '.emails-form:focus {outline: 0; }',
+    //         '.emails-form__input {position: relative; margin: 8px; min-width: 60px; flex: 1; display: block; }',
+    //         '.emails-form__input:empty::before {position: absolute; content: attr(data-placeholder); color: #C3C2CF; ' +
+    //             'top: 0; bottom: 0; white-space: nowrap; }',
+    //         '.emails-form__input:focus {outline: 0; }'
+    //     ]
+    // },
 };
-
-function EmailsInput(inputContainer, settings) {
-    var parser = new DOMParser(),
-        emailsForm = parser.parseFromString(templates.main(), "text/html");
-    inputContainer.appendChild(emailsForm.body.firstElementChild);
-    // inputContainer.parentNode.insertBefore(emailsForm.body.firstElementChild, inputContainer);
-
-
-
-    // var style = document.createElement('style');
-    // style.innerHTML = `
-    //     #emails-input {
-    //         width: 400px;
-    //         height: 400px;
-    //         background-color: antiquewhite;
-    //     }
-    // `;
-    // document.head.appendChild(style);
-}
-
-
-
-
-
-
-// (function(window, document, name) {
-//
-//     var templates = {
-//         main: function () {
-//             return '<div class="emails-form" \
-//                     tabIndex="-1"> \
-//                     <span contenteditable \
-//                         class="emails-form__input" \
-//                         data-placeholder="add more people..." \
-//                         aria-placeholder="add more people..."></span> \
-//                 </div> \
-//         ';
-//         }
-//     };
-//
-//     (function () {
-//
-//     })(window[name] = function (inputContainer, settings) {
-// debugger
-//         if (!inputContainer) return;
-//
-//         var parser = new DOMParser(),
-//             emailsForm = parser.parseFromString(templates.main(), "text/html");
-//         inputContainer.appendChild(emailsForm.body.firstElementChild);
-//     })
-//
-//     // window[name] = function (inputContainer, settings) {
-//     //
-//     //     if (!inputContainer) return;
-//     //
-//     //     var parser = new DOMParser(),
-//     //         emailsForm = parser.parseFromString(templates.main(), "text/html");
-//     //     inputContainer.appendChild(emailsForm.body.firstElementChild);
-//     // };
-//
-//
-//
-//     // inputContainer.parentNode.insertBefore(emailsForm.body.firstElementChild, inputContainer);
-//
-//
-//
-//     // var style = document.createElement('style');
-//     // style.innerHTML = `
-//     //     #emails-input {
-//     //         width: 400px;
-//     //         height: 400px;
-//     //         background-color: antiquewhite;
-//     //     }
-//     // `;
-//     // document.head.appendChild(style);
-// })(window, document, 'EmailsInput');
